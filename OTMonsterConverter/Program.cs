116fd6c878a3b54b83500c511b0336461048c0ac
soul4soul <soul4soul@localhost>
2014-11-01 19:17:59 -0400

    * improved code to be OO * support parsing of melee based attacks * fix to ignore filter in order to work with nested output directories

--- OTMonsterConverter/Program.cs
+++ OTMonsterConverter/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +12,7 @@ namespace OTMonsterConverter
         static void Main(string[] args)
         {
             Console.Write("Enter Monster File to Parse: ");
-            string inputDirectory = Console.ReadLine();
+            string inputFile = Console.ReadLine();
 
             //string[] array2 = Directory.GetFiles(inputDirectory, "*.xml");
             //enumerate files?
@@ -21,40 +20,8 @@ namespace OTMonsterConverter
             //Console.Write("Enter Output Directory: ");
             //string outputPath = Console.ReadLine();
 
-            Monster monster;
-            ReadMonster(inputDirectory, out monster);
-        }
-
-        private static void ReadMonster(string filename, out Monster monster)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(Monster));
-
-            serializer.UnknownNode += new XmlNodeEventHandler(serializer_UnknownNode);
-            serializer.UnknownAttribute += new XmlAttributeEventHandler(serializer_UnknownAttribute);
-
-            // A FileStream is needed to read the XML document.
-            FileStream fs = new FileStream(filename, FileMode.Open);
-
-            /* Use the Deserialize method to restore the object's state with
-            data from the XML document. */
-            monster = (Monster)serializer.Deserialize(fs);
-        }
-
-        private static void serializer_UnknownNode(object sender, XmlNodeEventArgs e)
-        {
-            Console.WriteLine("Unknown Node:" + e.Name + "\t" + e.Text);
-        }
-
-        private static void serializer_UnknownAttribute(object sender, XmlAttributeEventArgs e)
-        {
-            System.Xml.XmlAttribute attr = e.Attr;
-            Console.WriteLine("Unknown attribute " +
-            attr.Name + "='" + attr.Value + "'");
-        }
-
-        private static void WriteMonster(ref Monster monster)
-        {
-
+            TFSMonster monster = new TFSMonster();
+            monster.ReadMonster(inputFile);
         }
     }
 }
