966f2b5b09ccaefd5268e4f3eaf788de391e0112
soul4soul <soul4soul@localhost>
2014-11-02 13:28:12 -0500

    * more generic fields * better file naming scheme, * experiment with how pyOT writefile would look

--- OTMonsterConverter/GenericMonster.cs
+++ OTMonsterConverter/GenericMonster.cs
@@ -9,15 +9,79 @@ namespace OTMonsterConverter
     interface IGenericMonster
     {
         void ReadMonster(string filename);
-        void WriteMonster();
+        void WriteMonster(string filename);
     }
+
+    public class DetailedLookType
+    {
+        public uint head;
+        public uint body;
+        public uint legs;
+        public uint feet;
+    }
+
+    //public class Summon
+    //{
+    //    public string name;
+    //    public uint rate;
+    //    public uint chance;
+    //}
+
+    //public class Loot
+    //{
+    //    //todo: name count be ID or string
+    //    public uint max;
+    //    public uint chance;
+    //}
 
     abstract class GenericMonster : IGenericMonster
     {
+        // General
         public string name;
         public string description;
-        public int health;
+        public uint health;
         public List<string> voices;
+        public uint experience;
+        public uint speed;
+        //public uint maxSummons;
+        //race or blood?
+        //public List<Summon> summons;
+
+        // Look
+        public uint corpseId;
+        public uint lookId;
+        public DetailedLookType lookTypeDetails;
+
+        // Behavior
+        public uint summonable;
+        public bool hostile;
+        public bool illusionable;
+        public uint convinceable;
+        public bool pushable;
+        public bool pushItems;
+        public bool pushCreatures;
+        public bool targetDistance;
+        public uint runOnHealth;
+
+        // Defeneses
+        public uint armor;
+        public uint fire;
+        public uint earth;
+        public uint energy;
+        public uint ice;
+        public uint holy;
+        public uint death;
+        public uint physical;
+        public uint drown;
+
+        // Immunities
+        public bool paralyze;
+        public bool invisible;
+        public bool lifeDrain;
+        public bool drunk;
+        public bool manaDrain;
+
+
 
         public GenericMonster()
         {
@@ -26,6 +90,6 @@ namespace OTMonsterConverter
 
         public abstract void ReadMonster(string filename);
 
-        public abstract void WriteMonster();
+        public abstract void WriteMonster(string filename);
     }
 }
---
+++ OTMonsterConverter/PyOTMonster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTMonsterConverter
+{
+    //https://bitbucket.org/vapus/pyot/src/0aa7c38f46814f502f375b84ac905e7f5ebef1a3/game/monster.py?at=default
+
+    class PyOTMonster : GenericMonster
+    {
+        // Constructor
+        public PyOTMonster()
+            : base()
+        {
+        }
+
+                // Functions
+        public override void ReadMonster(string filename)
+        {
+        }
+
+        public override void WriteMonster(string filename)
+        {
+            string lowerName = this.name.ToLower();
+
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            string titleName = textInfo.ToTitleCase(lowerName); //War And Peace
+
+            string[] lines =
+            {
+                string.Format("{0} = genMonster(\"{1}\", ({2}, {3}), \"{4}\")", lowerName, titleName, this.corpseId, this.lookId, this.description),
+                string.Format("{0}.setHealth({1})", lowerName, this.health),
+                //string.Format("{0}.bloodType({1})", lowerName, this. ),
+                //string.Format("{0}.setDefense({1})", lowerName, this. ),
+                string.Format("{0}.setExperience({1})", lowerName, this.experience),
+                string.Format("{0}.setSpeed({1})", lowerName, this.speed),
+                //string.Format("{0}.setBehavior({1})", lowerName, this. ),
+                //string.Format("{0}.walkAround({1})", lowerName, this. ),
+                //string.Format("{0}.summon({1})", lowerName, this.health),
+                //string.Format("{0}.maxSummons({1})", lowerName, this.health),
+                //string.Format("{0}.voices({1})", lowerName, this.health),
+                //string.Format("{0}.loot({1})", lowerName, this.health),
+            };
+            File.WriteAllLines(@filename, lines);
+        }
+    }
+}
