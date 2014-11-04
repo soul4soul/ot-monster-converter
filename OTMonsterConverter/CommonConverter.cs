47c6a909771402ab04aedf2462518bb89356ae97
soul4soul <5142635+soul4soul@users.noreply.github.com>
2014-11-03 19:26:36 -0500

    * Changed inhertance heirarchy again, only conversion needs to be in related classes. Monster stucutre will `always` be the same * Remove some memeber variables of the Monster class in favor of properties, this will give us more control in the future if we need it.

---
+++ OTMonsterConverter/CommonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTMonsterConverter
+{
+    public interface ICommonConverter
+    {
+        void ReadMonster(string filename, out GenericMonster monster);
+
+        void WriteMonster(string filename, ref GenericMonster monster);
+    }
+
+    public abstract class CommonConverter : ICommonConverter
+    {
+        public abstract void ReadMonster(string filename, out GenericMonster monster);
+
+        public abstract void WriteMonster(string filename, ref GenericMonster monster);
+    }
+}
