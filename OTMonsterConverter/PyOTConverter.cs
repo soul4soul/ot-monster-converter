ce8651604246b355949482cae46e77205bda00df
soul4soul <5142635+soul4soul@users.noreply.github.com>
2014-11-09 13:51:44 -0500

    * fix boolean printouts for pyOT monster file writing * added support for targetchance

--- OTMonsterConverter/PyOTConverter.cs
+++ OTMonsterConverter/PyOTConverter.cs
@@ -30,7 +30,6 @@ namespace OTMonsterConverter
 
             TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
             string titleName = textInfo.ToTitleCase(lowerName);
-
             string[] lines =
             {
                 string.Format("{0} = genMonster(\"{1}\", ({2}, {3}), \"{4}\")", lowerName, titleName, monster.CorpseId, monster.OutfitIdLookType, monster.Description),
@@ -40,15 +39,30 @@ namespace OTMonsterConverter
                     lowerName, monster.TotalArmor, monster.Fire, monster.Earth, monster.Energy, monster.Ice, monster.Holy, monster.Death, monster.Physical, monster.Drown),
                 string.Format("{0}.setExperience({1})", lowerName, monster.Experience),
                 string.Format("{0}.setSpeed({1})", lowerName, monster.Speed),
-                string.Format("{0}setBehavior(summonable={1}, hostile={2}, illusionable={3}, convinceable={4}, pushable={5}, pushItems={6}, pushCreatures={7}, targetDistance={8}, runOnHealth={9})",
-                    lowerName, monster.SummonCost, monster.Hostile.ToString(), monster.Illusionable.ToString(), monster.Pushable.ToString(), monster.PushItems.ToString(), monster.PushCreatures.ToString(), monster.TargetDistance, monster.RunOnHealth),
+                //string.Format("{0}.setTargetChance({1})", lowerName, monster.retargetChance), //Currently unused in pyOT, consider changing code in pyOT to be chance of 0 to 1 for a percent instead of 0 to 100
+                string.Format("{0}setBehavior(summonable={1}, hostile={2}, illusionable={3}, convinceable={4}, pushable={5}, pushItems={6}, pushCreatures={7}, targetDistance={8}, runOnHealth={9}, targetChange={10})",
+                    lowerName, monster.SummonCost, boolToInt(monster.Hostile), boolToInt(monster.Illusionable), boolToInt(monster.Pushable), boolToInt(monster.PushItems), boolToInt(monster.PushCreatures), monster.TargetDistance, monster.RunOnHealth, 1),
                 string.Format("{0}.walkAround(energy={1}, fire={2}, poison={3})", lowerName, monster.avoidEnergy, monster.avoidFire, monster.avoidPoison),
-                //string.Format("{0}.summon({1})", lowerName, monster.health),
+                string.Format("{0}.setImmunity(paralyze={1}, invisible={2}, lifedrain={3}, drunk={4})",
+                    lowerName, boolToInt(monster.IgnoreParalyze), boolToInt(monster.IgnoreInvisible), 0, monster.IgnoreDrunk),
                 //string.Format("{0}.maxSummons({1})", lowerName, monster.health),
+                //string.Format("{0}.summon({1})", lowerName, monster.health),
                 //string.Format("{0}.voices({1})", lowerName, monster.health),
                 //string.Format("{0}.loot({1})", lowerName, monster.health),
             };
             File.WriteAllLines(@filename, lines);
+        }
+
+        private ushort boolToInt(bool value)
+        {
+            if (value)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         private string generictoPyOTBlood(Blood race)
