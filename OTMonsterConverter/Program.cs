5d35fcb97093c72dcbd5378334ede931f4d3588b
soul4soul <5142635+soul4soul@users.noreply.github.com>
2019-04-20 20:18:13 -0400

    Create scaffolding for using mono.options for program input to be run form the CLI

--- OTMonsterConverter/Program.cs
+++ OTMonsterConverter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,68 @@ namespace OTMonsterConverter
 {
     class Program
     {
+        public enum MonsterFormats
+        {
+            PyOT,
+            TfsXml,
+            TfsRevScriptSys
+        }
+
         static void Main(string[] args)
         {
+            bool show_help = false;
+            string inputDirectory;
+            int desiredFormat;
+            string outputDirectory;
+            bool mirrorFolderStructure = true;
+
+            var p = new OptionSet()
+            {
+                "Usage: greet [OPTIONS]+ message",
+                "Greet a list of individuals with an optional message.",
+                "If no message is specified, a generic greeting is used.",
+                "",
+                "Options:",
+                { "i|inputDirectory=", "The directory of monster files to parse.", v => inputDirectory = v },
+                { "o|outputDirectory=", "The directory of monster files to parse.", v => outputDirectory = v },
+                { "desiredFormat=", "The format to converter the monster files to.", (int v) => desiredFormat = v },
+                { "m|MirrorFolders", "True to mirror the folder structure of the input directory", v => mirrorFolderStructure = v != null },
+                { "h|help",  "show this message and exit", v => show_help = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("greet: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `greet --help' for more information.");
+                return;
+            }
+
+            if (show_help)
+            {
+                p.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            // Call functions from converter class
+            // Get list of files?
+            // Get format from extensions?
+            // Do for each file:
+            ///
+            // Read in file
+            //  Print error for any unknown variables
+            // Print out file
+            //   Print error for any unknown variables
+            // For each file output new file
+            //  Print out erro
+            ///
+            // Done?
+
             Console.Write("Enter Monster File to Parse: ");
             string inputFile = Console.ReadLine();
 
@@ -20,7 +81,7 @@ namespace OTMonsterConverter
             //Console.Write("Enter Output Directory: ");
             //string outputPath = Console.ReadLine();
 
-            ICommonConverter tfsConverter = new TFSConverter();
+            ICommonConverter tfsConverter = new TfsXmlConverter();
             ICustomMonster monster;
             tfsConverter.ReadMonster(inputFile, out monster);
         }
