3a40d45da54eb52d7b9f73fa09affb62828b70ff
soul4soul <5142635+soul4soul@users.noreply.github.com>
2019-04-21 11:12:17 -0400

    Improve parsing of TFS create, most values are not using the default from TFS when not explicitly defined in the XML file

--- OTMonsterConverter/TfsXmlConverter.cs
+++ OTMonsterConverter/TfsXmlConverter.cs
@@ -58,17 +58,16 @@ namespace OTMonsterConverter
         private void xmlToGeneric(TFSXmlMonster tfsMonster, out ICustomMonster monster)
         {
             monster = new CustomMonster()
-                        {
-                            Name           = tfsMonster.name,
-                            Description    = tfsMonster.nameDescription,
-                            Health         = (uint)tfsMonster.health.max,
-                            Experience     = (uint)tfsMonster.experience,
-                            Speed          = (uint)tfsMonster.speed,
-                            CorpseId       = (uint)tfsMonster.look.corpse,
-                            Race           = tfsToGenericBlood(tfsMonster.race),
-                            RetargetChance = (uint)tfsMonster.targetchange.chance,
-                            MaxSummons     = (uint)tfsMonster.summons.maxSummons
-                        };
+            {
+                Name           = tfsMonster.name,
+                Description    = tfsMonster.nameDescription,
+                Health         = (uint)tfsMonster.health.max,
+                Experience     = (uint)tfsMonster.experience,
+                Speed          = (uint)tfsMonster.speed,
+                CorpseId       = (uint)tfsMonster.look.corpse,
+                Race           = tfsToGenericBlood(tfsMonster.race),
+                RetargetChance = (uint)tfsMonster.targetchange.chance,
+            };
 
             if (tfsMonster.look.type != 0)
             {
@@ -95,8 +94,9 @@ namespace OTMonsterConverter
             }
 
             // summons
-            if (tfsMonster.summons.summon != null)
+            if (tfsMonster.summons != null)
             {
+                monster.MaxSummons = (uint)tfsMonster.summons.maxSummons;
                 foreach (TFSXmlSummon summon in tfsMonster.summons.summon)
                 {
                     monster.Summons.Add(new CustomSummon()
@@ -107,59 +107,49 @@ namespace OTMonsterConverter
                                             });
                 }
             }
+            else
+            {
+                monster.MaxSummons = 0;
+            }
 
             // Defenses
             monster.TotalArmor = tfsMonster.defenses.armor;
             monster.TotalArmor = tfsMonster.defenses.defense;
+
             #region parseElements
-            foreach (Element element in tfsMonster.elements.element)
+            monster.Physical = tfstoGenericElementalPercent(tfsMonster.elements.element[0].physicalPercent);
+            monster.Ice = tfstoGenericElementalPercent(tfsMonster.elements.element[0].icePercent);
+            if ((tfsMonster.elements.element[0].poisonPercent != 0) &&
+                    (tfsMonster.elements.element[0].earthPercent != 0))
             {
-                if (element.physicalPercent != -9999)
-                {
-                    monster.Physical = tfstoGenericElementalPercent(element.physicalPercent);
-                }
-                else if (element.icePercent != -9999)
-                {
-                    monster.Ice = tfstoGenericElementalPercent(element.icePercent);
-                }
-                else if (element.poisonPercent != -9999) //poisonPercent and earthPercent are the same
-                {
-                    monster.Earth = tfstoGenericElementalPercent(element.poisonPercent);
-                }
-                else if (element.earthPercent != -9999) //poisonPercent and earthPercent are the same
-                {
-                    monster.Earth = tfstoGenericElementalPercent(element.earthPercent);
-                }
-                else if (element.firePercent != -9999)
-                {
-                    monster.Fire = tfstoGenericElementalPercent(element.firePercent);
-                }
-                else if (element.energyPercent != -9999)
-                {
-                    monster.Energy = tfstoGenericElementalPercent(element.energyPercent);
-                }
-                else if (element.holyPercent != -9999)
-                {
-                    monster.Holy = tfstoGenericElementalPercent(element.holyPercent);
-                }
-                else if (element.deathPercent != -9999)
-                {
-                    monster.Death = tfstoGenericElementalPercent(element.deathPercent);
-                }
-                else if (element.drownPercent != -9999)
-                {
-                    monster.Drown = tfstoGenericElementalPercent(element.drownPercent);
-                }
-                else if (element.lifedrainPercent != -9999)
-                {
-                    monster.LifeDrain = tfstoGenericElementalPercent(element.lifedrainPercent);
-                }
-                else if (element.manadrainPercent != -9999)
-                {
-                    monster.ManaDrain = tfstoGenericElementalPercent(element.manadrainPercent);
-                }
+                Console.WriteLine("Warning duplicate poisonPercent and earthPercent defined");
             }
+            else if ((tfsMonster.elements.element[0].poisonPercent != 0) &&
+                    (tfsMonster.elements.element[0].earthPercent == 0))
+            {
+                monster.Earth = tfstoGenericElementalPercent(tfsMonster.elements.element[0].poisonPercent);
+
+            }
+            else if ((tfsMonster.elements.element[0].poisonPercent == 0) &&
+                     (tfsMonster.elements.element[0].earthPercent != 0))
+            {
+                monster.Earth = tfstoGenericElementalPercent(tfsMonster.elements.element[0].earthPercent);
+
+            }
+            else
+            {
+                monster.Earth = 1;
+            }
+            monster.Earth = tfstoGenericElementalPercent(tfsMonster.elements.element[0].earthPercent);
+            monster.Fire = tfstoGenericElementalPercent(tfsMonster.elements.element[0].firePercent);
+            monster.Energy = tfstoGenericElementalPercent(tfsMonster.elements.element[0].energyPercent);
+            monster.Holy = tfstoGenericElementalPercent(tfsMonster.elements.element[0].holyPercent);
+            monster.Death = tfstoGenericElementalPercent(tfsMonster.elements.element[0].deathPercent);
+            monster.Drown = tfstoGenericElementalPercent(tfsMonster.elements.element[0].drownPercent);
+            monster.LifeDrain = tfstoGenericElementalPercent(tfsMonster.elements.element[0].lifedrainPercent);
+            monster.ManaDrain = tfstoGenericElementalPercent(tfsMonster.elements.element[0].manadrainPercent);
             #endregion
+
             #region paraseImmunities
             foreach (Immunity immunity in tfsMonster.immunities.immunity)
             {
@@ -435,15 +425,15 @@ namespace OTMonsterConverter
         [XmlAttribute]
         public string nameDescription;
         [XmlAttribute]
-        public string race; //todo: this could be number or string
+        public string race = "blood"; //todo: this could be number or string
         [XmlAttribute]
-        public int experience;
+        public int experience = 0;
         [XmlAttribute]
-        public int speed;
+        public int speed = 200;
         [XmlAttribute]
-        public int manacost;
+        public int manacost = 0;
         [XmlAttribute]
-        public string skull; //uses strings, "none", "yellow", "green", "white", "red", "black", "orange"
+        public string skull = "none"; //uses strings, "none", "yellow", "green", "white", "red", "black", "orange"
         //[XmlAttribute]
         //public int script; //todo: how to handle
 
@@ -464,11 +454,10 @@ namespace OTMonsterConverter
     public class TFSXmlHealth
     {
         [XmlAttribute]
-        public int now;
+        public int now = 100;
         [XmlAttribute]
-        public int max;
+        public int max = 100;
     }
-
 
     [XmlRoot(ElementName = "flags")]
     public class Flags
@@ -480,23 +469,23 @@ namespace OTMonsterConverter
     public class Flag
     {
         [XmlAttribute]
-        public int summonable = -99;
+        public int summonable = 0;
         [XmlAttribute]
-        public int attackable = -99;
+        public int attackable = 1;
         [XmlAttribute]
-        public int hostile = -99;
+        public int hostile = 1;
         [XmlAttribute]
-        public int illusionable = -99;
+        public int illusionable = 0;
         [XmlAttribute]
-        public int convinceable = -99;
+        public int convinceable = 0;
         [XmlAttribute]
-        public int pushable = -99;
+        public int pushable = 1;
         [XmlAttribute]
-        public int canpushitems = -99;
+        public int canpushitems = 0;
         [XmlAttribute]
-        public int canpushcreatures = -99;
+        public int canpushcreatures = 0;
         [XmlAttribute]
-        public int targetdistance = -99;
+        public int targetdistance = 1;
         [XmlAttribute]
         public int staticattack = -99;
         [XmlAttribute]
@@ -504,9 +493,15 @@ namespace OTMonsterConverter
         [XmlAttribute]
         public int lightcolor = -99;
         [XmlAttribute]
-        public int runonhealth = -99;
+        public int runonhealth = 0;
         [XmlAttribute]
-        public int hidehealth = -99;
+        public int hidehealth = 0;
+        [XmlAttribute]
+        public int canwalkonenergy = 1;
+        [XmlAttribute]
+        public int canwalkonfire = 1;
+        [XmlAttribute]
+        public int canwalkonpoison = 1;
     }
 
     public class TargetChange
@@ -522,23 +517,23 @@ namespace OTMonsterConverter
     public class Look
     {
         [XmlAttribute]
-        public int type = -99;
+        public int type = 0;
         [XmlAttribute]
-        public int head = -99; //only can exist if type exists
+        public int head = 0; //only can exist if type exists
         [XmlAttribute]
-        public int body = -99; //only can exist if type exists
+        public int body = 0; //only can exist if type exists
         [XmlAttribute]
-        public int legs = -99; //only can exist if type exists
+        public int legs = 0; //only can exist if type exists
         [XmlAttribute]
-        public int feet = -99; //only can exist if type exists
+        public int feet = 0; //only can exist if type exists
         [XmlAttribute]
-        public int addons = -99; //only can exist if type exists
+        public int addons = 0; //only can exist if type exists
         [XmlAttribute]
-        public int typeex;
+        public int typeex = 0;
         [XmlAttribute]
-        public int mount;
+        public int mount = 0;
         [XmlAttribute]
-        public int corpse;
+        public int corpse = 0;
     }
 
     public class Attacks
@@ -549,28 +544,32 @@ namespace OTMonsterConverter
 
     public class Attack
     {
-        //[XmlAttribute]
-        //public int script; //todo: how to handle
+        // only script or name not both
+        [XmlAttribute]
+        public string script;
         [XmlAttribute]
         public string name;
-        public int interval = 2000; //interval and speed are the same //defaults to 2000 if missing
+
+        // Only one should exist, they represent the same information
         [XmlAttribute]
-        public int speed = 2000; //interval and speed are the same //defaults to 2000 if missing
+        public int interval = 2000; //defaults to 2000 if missing
+        [XmlAttribute]
+        public int speed = 2000; //defaults to 2000 if missing
 
-        //[XmlAttribute]
-        //public int chance = 100; //defaults to 100 if missing
-        //[XmlAttribute]
-        //public int range = 0; //defaults to 0 if missing
-        //[XmlAttribute]
-        //public int min = 0; //defaults to 0 if missing
-        //[XmlAttribute]
-        //public int max = 0; //defaults to 0 if missing
-        //[XmlAttribute]
-        //public int length = 0; //if length exists spread defaults to 3
-        //[XmlAttribute]
-        //public int radius = 0;
+        [XmlAttribute]
+        public int chance = 100; //defaults to 100 if missing
+        [XmlAttribute]
+        public int range = 0; //defaults to 0 if missing
+        [XmlAttribute]
+        public int min = 0; //defaults to 0 if missing
+        [XmlAttribute]
+        public int max = 0; //defaults to 0 if missing
+        [XmlAttribute]
+        public int length = 0; //if length exists spread defaults to 3
+        [XmlAttribute]
+        public int radius = 0;
 
-        // the following only exist if name is melee
+        // the following only exist when attack name is melee
         // when melee exists minMax and Max are set to 0
         // interval is set to 200
         [XmlAttribute]
@@ -620,39 +619,39 @@ namespace OTMonsterConverter
         [XmlAttribute]
         public namedImmunityXml name = namedImmunityXml.NA;
         [XmlAttribute]
-        public int physical = -9999;
+        public int physical = 0; //Immune to physical and bleeding condition
         [XmlAttribute]
-        public int energy = -9999;
+        public int energy = 0;
         [XmlAttribute]
-        public int fire = -9999;
+        public int fire = 0;
         [XmlAttribute]
-        public int poison = -9999; //poison and earth are the same
+        public int poison = 0; //poison and earth are the same
         [XmlAttribute]
-        public int earth = -9999; //poison and earth are the same
+        public int earth = 0; //poison and earth are the same
         [XmlAttribute]
-        public int drown = -9999;
+        public int drown = 0;
         [XmlAttribute]
-        public int ice = -9999;
+        public int ice = 0;
         [XmlAttribute]
-        public int holy = -9999;
+        public int holy = 0;
         [XmlAttribute]
-        public int death = -9999;
+        public int death = 0;
         [XmlAttribute]
-        public int lifedrain = -9999;
+        public int lifedrain = 0;
         [XmlAttribute]
-        public int manadrain = -9999;
+        public int manadrain = 0;
         [XmlAttribute]
-        public int paralyze = -9999;
+        public int paralyze = 0;
         [XmlAttribute]
-        public int outfit = -9999;
+        public int outfit = 0;
         [XmlAttribute]
-        public int bleed = -9999;
+        public int bleed = 0; // immue to only bleed condition
         [XmlAttribute]
-        public int drunk = -9999;
+        public int drunk = 0;
         [XmlAttribute]
-        public int invisible = -9999; //invisible and invisibility are the same
+        public int invisible = 0; //invisible and invisibility are the same
         [XmlAttribute]
-        public int invisibility = -9999; //invisible and invisibility are the same
+        public int invisibility = 0; //invisible and invisibility are the same
     }
 
     public class Voices
@@ -683,6 +682,9 @@ namespace OTMonsterConverter
 
     public class Item
     {
+        // Only name or ID will be used not both
+        [XmlAttribute]
+        public string name;
         [XmlAttribute]
         public string id;
         [XmlAttribute]
@@ -707,30 +709,31 @@ namespace OTMonsterConverter
         public Element[] element;
     }
 
+    // Default is 0 which means 100% damage, negative number is 100% minus negative,
     public class Element
     {
         [XmlAttribute]
-        public int physicalPercent = -9999;
+        public int physicalPercent = 0;
         [XmlAttribute]
-        public int icePercent = -9999;
+        public int icePercent = 0;
         [XmlAttribute]
-        public int poisonPercent = -9999; //poisonPercent and earthPercent are the same
+        public int poisonPercent = 0; //poisonPercent and earthPercent are the same
         [XmlAttribute]
-        public int earthPercent = -9999; //poisonPercent and earthPercent are the same
+        public int earthPercent = 0; //poisonPercent and earthPercent are the same
         [XmlAttribute]
-        public int firePercent = -9999;
+        public int firePercent = 0;
         [XmlAttribute]
-        public int energyPercent = -9999;
+        public int energyPercent = 0;
         [XmlAttribute]
-        public int holyPercent = -9999;
+        public int holyPercent = 0;
         [XmlAttribute]
-        public int deathPercent = -9999;
+        public int deathPercent = 0;
         [XmlAttribute]
-        public int drownPercent = -9999;
+        public int drownPercent = 0;
         [XmlAttribute]
-        public int lifedrainPercent = -9999;
+        public int lifedrainPercent = 0;
         [XmlAttribute]
-        public int manadrainPercent = -9999;
+        public int manadrainPercent = 0;
     }
 
     [XmlRoot(ElementName = "summons")]
