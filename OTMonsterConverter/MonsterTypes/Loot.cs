9b64f5ca55e2a7113bb5ceec7ef7ca6b8e57b1e8
soul4soul <5142635+soul4soul@users.noreply.github.com>
2020-04-28 14:54:00 -0400

    Override tostring method of Loot class to make debugging easier
    In the watch window it's helpful to see all loot information as a string
    to avoid needing to browse into each loot item.

--- OTMonsterConverter/MonsterTypes/Loot.cs
+++ OTMonsterConverter/MonsterTypes/Loot.cs
@@ -9,5 +9,10 @@ namespace OTMonsterConverter.MonsterTypes
         public string Item { get; set; }
         public decimal Chance { get; set; }
         public decimal Count { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item} {Count} {Chance:N4}";
+        }
     }
 }
