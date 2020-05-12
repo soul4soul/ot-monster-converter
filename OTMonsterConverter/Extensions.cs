43d33a2e9116fbd72a4e9f08503b5f7e1d1666f6
soul4soul <5142635+soul4soul@users.noreply.github.com>
2020-05-12 17:48:06 -0400

    Tibiawiki parsing (#2)
    * Add ability to create creatures from information on tibiawiki.
    * Add ability to create tibiawiki creature pages using OT creature information.
    * Show the user an error when the input and output directories match
    * Allow input path to be empty when input type is tibiawiki
    
    Co-authored-by: soul4soul <email@domain.com>

---
+++ OTMonsterConverter/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OTMonsterConverter
+{
+    public static class Extensions
+    {
+        public static string FindNamedGroupValue(this MatchCollection mc, string name)
+        {
+            foreach (Match m in mc)
+            {
+                var group = m.Groups[name];
+                if (group != null)
+                {
+                    return group.Value;
+                }
+            }
+
+            throw new Exception($"Named group parameter {name} not found in MatchCollection");
+        }
+    }
+}
