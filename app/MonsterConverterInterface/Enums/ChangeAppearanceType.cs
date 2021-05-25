c7e86f856755f8b02fcfadb3e422f32841656790
soul4soul <5142635+soul4soul@users.noreply.github.com>
2021-05-25 11:15:21 -0400

    Fix many issues parsing and writing TFS monster abilities (#18)
    These changes fix issues #6, #8, and #14 (both the melee issue and combat damage type issue)
    
    Other improvements beyond the reported issues
    
        TFS XML: Fixed parsing of condition causing spells, melee attack name, duration, animations, effects, and area information for all spells
        TFS RevScriptSys: Writing condition causing spells, duration, area information, outfit changing spells, animations, and effects for all, and combat type
        TFSXML->TFS RevScriptSys: Spells from files now in some cases but needs more testing and checking overall

---
+++ app/MonsterConverterInterface/Enums/ChangeAppearanceType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterConverterInterface.MonsterTypes
+{
+    public enum ChangeAppearanceType
+    {
+        MonsterName,
+        OutfitId,
+        Item
+    }
+}
---
+++ app/MonsterConverterInterface/Enums/CombatDamage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterConverterInterface.MonsterTypes
+{
+    public enum CombatDamage
+    {
+        Physical,
+        Energy,
+        Earth,
+        Fire,
+        LifeDrain,
+        ManaDrain,
+        Healing,
+        Drown,
+        Ice,
+        Holy,
+        Death
+    }
+}
