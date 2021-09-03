4c975b83929d5e41cf81338db3ddf32ab285b4b2
soul4soul <5142635+soul4soul@users.noreply.github.com>
2021-09-03 15:01:34 -0400

    Add support for parsing bestiary information
    Fixes #22.
    At this time the information is only used for TibiaWiki

---
+++ app/MonsterConverterInterface/MonsterTypes/BestiaryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterConverterInterface.MonsterTypes
+{
+    public class BestiaryData
+    {
+        /// <summary>
+        /// TODO: Evalute, turning this into an enum. The name and number of them may be restricted by the client
+        /// </summary>
+        public string Class { get; set; }
+        public int CharmPoints { get; set; }
+        public int FirstDetailStage { get; set; }
+        public int SecondDetailStage { get; set; }
+        public int FinalDetailStage { get; set; }
+        public string Location { get; set; }
+        /// <summary>
+        /// Cip goes from 0 (harmless) to 5 (Challenging)
+        /// </summary>
+        public int DifficultlyStarCount { get; set; }
+        /// <summary>
+        /// Cip goes from 0 (common) to 3 (very rare)
+        /// </summary>
+        public int OccuranceDiamondCount { get; set; }
+    }
+}
