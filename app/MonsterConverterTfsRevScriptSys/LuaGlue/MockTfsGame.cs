f033cf40686ff574808b4ac2a0de84d1e6c7500f
soul4soul <5142635+soul4soul@users.noreply.github.com>
2023-02-13 09:09:41 -0500

    remove temp comments

--- app/MonsterConverterTfsRevScriptSys/LuaGlue/MockTfsGame.cs
+++ app/MonsterConverterTfsRevScriptSys/LuaGlue/MockTfsGame.cs
@@ -18,51 +18,13 @@ namespace MonsterConverterTfsRevScriptSys
         static Queue<Tuple<Monster, ConvertResultEventArgs>> convertedMonsters = new Queue<Tuple<Monster, ConvertResultEventArgs>>(1);
 
         public static Queue<Tuple<Monster, ConvertResultEventArgs>> ConvertedMonsters
-		{
-			private set { ; }
-			get { return convertedMonsters; }
-		}
+        {
+            private set { ; }
+            get { return convertedMonsters; }
+        }
 
         public static MockTfsMonsterType createMonsterType(string name)
         {
-			/*
-int LuaScriptInterface::luaGameCreateMonsterType(lua_State* L)
-{
-	// Game.createMonsterType(name)
-	if (getScriptEnv()->getScriptInterface() != &g_scripts->getScriptInterface()) {
-		reportErrorFunc(L, "MonsterTypes can only be registered in the Scripts interface.");
-		lua_pushnil(L);
-		return 1;
-	}
-
-	const std::string& name = getString(L, 1);
-	if (name.length() == 0) {
-		lua_pushnil(L);
-		return 1;
-	}
-
-	MonsterType* monsterType = g_monsters.getMonsterType(name, false);
-	if (!monsterType) {
-		monsterType = &g_monsters.monsters[boost::algorithm::to_lower_copy(name)];
-		monsterType->name = name;
-		monsterType->nameDescription = "a " + name;
-	} else {
-		monsterType->info.lootItems.clear();
-		monsterType->info.attackSpells.clear();
-		monsterType->info.defenseSpells.clear();
-		monsterType->info.scripts.clear();
-		monsterType->info.thinkEvent = -1;
-		monsterType->info.creatureAppearEvent = -1;
-		monsterType->info.creatureDisappearEvent = -1;
-		monsterType->info.creatureMoveEvent = -1;
-		monsterType->info.creatureSayEvent = -1;
-	}
-
-	pushUserdata<MonsterType>(L, monsterType);
-	setMetatable(L, -1, "MonsterType");
-	return 1;
-}
-			*/
             return new MockTfsMonsterType(name);
         }
     }
